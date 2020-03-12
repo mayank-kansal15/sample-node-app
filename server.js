d14d728e675dc7d4dbf4266840f2a340ae20aa60
mayank-kansal15 <mayank.kansal@talentica.com>
2020-03-12 06:19:45 +0530

    added error log api

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express')
+const fs = require("fs");
 const app = express()
 const port = 8000
+const logFilePath = "/sample-node-app/combined.log";
 
 app.get('/', (req, res) => {
     console.log("Hello world!");
@@ -8,7 +10,13 @@ app.get('/', (req, res) => {
 });
 app.get('/v1/app-name', (req, res) => {
     console.log("Sample-node-app");
+    fs.appendFileSync(logFilePath, "Sample-node-app\n");
     res.send('Sample-node-app');
+});
+app.get('/v1/generate-error-log', (req, res) => {
+  console.log("generate error log");
+  fs.appendFileSync(logFilePath, "ERROR: Some error happened on server\n");
+  res.send('Error generated');
 });
 app.get('/health', (req, res) => {
     res.send('I am healthy, thanks!')
